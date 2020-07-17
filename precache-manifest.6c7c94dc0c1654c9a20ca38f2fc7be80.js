e5925df86097d95fc2ebb5b3d27ea4430b8ed112
razorrahul123 <rahul.kr.sushant@gmail.com>
2020-07-17 13:20:37 +0530

    Updates

---
+++ precache-manifest.6c7c94dc0c1654c9a20ca38f2fc7be80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd6bd771957ec34d9f2b4a10f9d301e",
+    "url": "/college-react/index.html"
+  },
+  {
+    "revision": "4928b48b404d680cc01c",
+    "url": "/college-react/static/css/main.4c1eef3b.chunk.css"
+  },
+  {
+    "revision": "3683aacf1e739c5f77f7",
+    "url": "/college-react/static/js/2.4018c7d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/college-react/static/js/2.4018c7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4928b48b404d680cc01c",
+    "url": "/college-react/static/js/main.273556e3.chunk.js"
+  },
+  {
+    "revision": "03a0928c9fc96a6798be",
+    "url": "/college-react/static/js/runtime-main.469b5b5f.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/college-react/static/media/college_02.619ee97e.jpg"
+  }
+]);
